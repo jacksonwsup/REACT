2807ffe399faacf8101bf2fe851634e2d6739a0c
Jackson R <jacksonwsup@gmail.com>
2023-09-23 14:48:04 -0300

    Criado POST utilizando JS

--- blog/src/App.tsx
+++ blog/src/App.tsx
@@ -1,17 +1,24 @@
+import { NewPost } from "./components/NewPost";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+
 import { Home } from "./components/Home";
 import { Navbar } from "./components/Navbar";
 
 export function App() {
   
   return (
-    <>
+    <Router>
       <div className="App">
         <Navbar />
         <div className="content">
-          <Home />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<NewPost />} />
+          </Routes>
         </div>
       </div>
-    </>
+    </Router>
   )
 
 }
