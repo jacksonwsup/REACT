7c6801aa538f6a9da2ffc6e80d2c0a236df54a69
Jackson R <jacksonwsup@gmail.com>
2023-09-23 09:56:04 -0300

    Criando função

--- blog/src/components/Home.tsx
+++ blog/src/components/Home.tsx
@@ -1,10 +1,16 @@
 export function Home() {
   
-    const title = "Bem-vindo!";
+    let title = "Bem-vindo!"; 
+
+    function handleClick() {
+        title = "Tchau!"
+        //alert("Você clicou no botão");
+        alert(title);
+    }
   
     return (
       <div className="home">
-        <h2>{ title }</h2>
+        <h2 onClick={ handleClick }>{ title }</h2>
       </div>
     )
   
